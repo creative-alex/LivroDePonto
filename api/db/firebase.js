46332755efec1bfb0ea1c2dbcacc52001d386634
creative-alex <goncalves.antonio2002@gmail.com>
2025-05-02 09:24:23 +0100

    dt8

--- api/db/firebase.js
+++ api/db/firebase.js
@@ -1,9 +1,11 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(
-  process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON.replace(/\\n/g, '\n')
-);
+const serviceAccount = require('./serviceAccountKey.json');
 
+// Corrige o formato da chave privada
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
+// Use o objeto corrigido
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DATABASE_URL
