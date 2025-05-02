7498a5df2ab963655764c795375003b9ae2422de
creative-alex <goncalves.antonio2002@gmail.com>
2025-05-02 10:21:09 +0100

    deplaoy test

--- api/db/firebase.js
+++ api/db/firebase.js
@@ -1,6 +1,9 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require('./serviceAccountKey.json');
+
+// Corrige o formato da chave privada
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 // Use o objeto corrigido
 admin.initializeApp({
