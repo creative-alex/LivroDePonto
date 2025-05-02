e3d7e5cf14bf52089ed3640750fb85ee24dbb0bc
creative-alex <goncalves.antonio2002@gmail.com>
2025-05-02 09:13:08 +0100

    dt7

--- api/db/firebase.js
+++ api/db/firebase.js
@@ -1,5 +1,8 @@
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+
+const serviceAccount = JSON.parse(
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON.replace(/\\n/g, '\n')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
