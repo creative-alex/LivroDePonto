918d87746c31635c964bbf26946a26de4673c90d
creative-alex <goncalves.antonio2002@gmail.com>
2025-05-02 12:19:14 +0100

    Verificaçao de registo de entrada fixed

--- front-end/src/pages/MenuUser/buttons/entryRegisterButton.jsx
+++ front-end/src/pages/MenuUser/buttons/entryRegisterButton.jsx
@@ -6,38 +6,22 @@ const EntryButton = ({ username }) => {
   const [isDisabled, setIsDisabled] = useState(false);
 
   useEffect(() => {
-    if (!username) {
-      console.warn('Username não definido. Abortando verificação.');
-      return;
-    }
-
     const checkEntry = async () => {
       try {
-        console.log('Verificando entrada para o usuário:', username); // Log para depuração
-        const response = await fetch("https://livrodeponto-fd4b.onrender.com/users/checkEntry", {
+        const response = await fetch('https://api-ls3q.onrender.com/users/checkEntry', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({ username })
         });
-
-        if (!response.ok) {
-          throw new Error(`Erro na API: ${response.status}`);
-        }
-
+        
         const data = await response.json();
-        console.log('Resposta da API:', data); // Log para depuração
         if (data.hasEntry) {
           setIsDisabled(true);
         }
       } catch (error) {
-        console.error('Erro ao verificar entrada:', error);
-        if (error.message.includes('500')) {
-          toast.error('Erro interno no servidor. Tente novamente mais tarde.', { position: 'top-right', autoClose: 3000 });
-        } else {
-          toast.error('Erro ao verificar entrada. Tente novamente mais tarde.', { position: 'top-right', autoClose: 3000 });
-        }
+        console.error('Erro ao verificar saída:', error);
       }
     };
 
